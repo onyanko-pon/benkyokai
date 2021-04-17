d754d664131a0fd99709be19b0f91f8fc3eb663d
tsukasa-forever <engineer.tsukasa@gmail.com>
2021-04-17 17:43:01 +0900

    sequelize

--- db/sequelize.js
+++ db/sequelize.js
@@ -6,20 +6,23 @@ console.log("DB_PASSWORD", process.env.DB_PASSWORD)
 console.log("DB_HOST", process.env.DB_HOST)
 console.log("DB_PORT", process.env.DB_PORT)
 
-// TODO 環境変数からとる
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  // dialect: 'mysql'|'mariadb'|'sqlite'|'postgres'|'mssql',
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-  define: {
-    timestamps: false
-  }
-});
-
-module.exports = sequelize
+if (process.env.NODE_ENV === "production") {
+  const sequelize = new Sequelize(process.env.DATABASE_URL)
+  module.exports = sequelize
+} else {
+  const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    // dialect: 'mysql'|'mariadb'|'sqlite'|'postgres'|'mssql',
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+    define: {
+      timestamps: false
+    }
+  });
+  module.exports = sequelize
+}
