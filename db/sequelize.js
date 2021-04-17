998769e3cde5ff1df50b19e3d5d510bc461afe6c
tsukasa-forever <engineer.tsukasa@gmail.com>
2021-04-17 12:35:53 +0900

    add lkog

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,5 +1,10 @@
 const Sequelize = require('sequelize');
 
+console.log("DB_NAME", process.env.DB_NAME)
+console.log("DB_USERNAME", process.env.DB_USERNAME)
+console.log("DB_PASSWORD", process.env.DB_PASSWORD)
+console.log("DB_HOST", process.env.DB_HOST)
+console.log("DB_PORT", process.env.DB_PORT)
 
 // TODO 環境変数からとる
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
