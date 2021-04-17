44806a8f646178ee5123fa5b9793eef27c2cba32
tsukasa-forever <engineer.tsukasa@gmail.com>
2021-04-17 17:56:33 +0900

    fix ssl connect to db

--- db/sequelize.js
+++ db/sequelize.js
@@ -8,7 +8,11 @@ console.log("DB_PORT", process.env.DB_PORT)
 console.log("DATABASE_URL", process.env.DATABASE_URL)
 
 if (process.env.NODE_ENV === "production") {
-  const sequelize = new Sequelize(process.env.DATABASE_URL)
+  const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: true
+    }
+  })
   module.exports = sequelize
 } else {
   const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
