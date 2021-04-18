0435f0b041c303e1cd3de60177f249a29f74b69d
tsukasa-forever <engineer.tsukasa@gmail.com>
2021-04-19 00:28:31 +0900

    削除と更新

--- routes/api/event.js
+++ routes/api/event.js
@@ -12,4 +12,31 @@ router.get("/", (req, res) => {
   })
 })
 
+router.put("/:event_id", (req, res) => {
+  const {event_id} = req.params
+  const {description, title} = req.body
+
+  Event.findByPk(event_id)
+    .then(async (event) => {
+      event.description = description
+      event.title = title
+      await event.save()
+      res.status(200).json({event})
+    })
+    .catch(error => {
+      res.status(500).json({error})
+    })
+})
+
+router.delete("/:event_id", (req, res) => {
+  const {event_id} = req.params
+  Event.destroy({
+    where: {id: event_id}
+  }).then(value => {
+    res.status(200).json({value})
+  }).catch(error => {
+    res.status(500).json({error})
+  })
+})
+
 module.exports = router;
