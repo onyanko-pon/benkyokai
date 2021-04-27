9c354d1ebf248f878ae9a761edbd4eda867d8989
tsukasa-forever <engineer.tsukasa@gmail.com>
2021-04-28 00:25:49 +0900

    add ok variable to response

--- routes/slack/auth.js
+++ routes/slack/auth.js
@@ -5,23 +5,20 @@ const fetch = require('node-fetch')
 router.post("/v2.access", async (req, res) => {
   const { code } = req.body
 
-  fetch("https://slack.com/api/oauth.v2.access", {
+  const data = (await fetch("https://slack.com/api/oauth.v2.access", {
     method: "POST",
     headers: {"Content-Type": "application/x-www-form-urlencoded"},
     body: `code=${code}&client_id=${process.env.SLACK_API_CLIENT_ID}&client_secret=${process.env.SLACK_API_CLIENT_SECRET}`
-  }).then(data => data.json())
-    .then(data => {
-      const {authed_user, team, ok} = data
-      if (ok) {
-        res.status(200).json({authed_user, team})
-      } else {
-        res.status(401).json(data)
-      }
-    })
-    .catch(error => {
-      res.status(500).json({error})
-    })
+  })).json()
 
+  const {authed_user, team, ok} = data
+
+  if (!ok) {
+    res.status(401).json(data)
+    return
+  }
+
+  res.status(200).json({authed_user, team, ok})
 })
 
 router.get("/hello", (req, res) => {
