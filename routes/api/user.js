22a26923e7a0fce61bd205a1a14ecd2f89e342a7
tsukasa-forever <engineer.tsukasa@gmail.com>
2021-05-05 04:41:04 +0900

    delete endpoint for user get

--- routes/api/user.js
+++ routes/api/user.js
@@ -4,34 +4,8 @@ const auth = require('../../auth')
 
 const { Event, User } = require('../../models')
 
-router.get("/:userId", auth, async (req, res) => {
-  const { userId } = req.params
-  const user = await User.findByPk(userId, {
-    include: [
-      {
-        model: Event,
-      },
-      {
-        model: Event,
-        as: 'AdminEvent'
-      },
-    ],
-  })
-
-  if (!user) {
-    return res.status(404).json({message: "not found"})
-  }
-
-  if (user.id !== parseInt(req.jwtPayload.user_id)) {
-    return res.status(403).json({message: "not auth"})
-  }
-
-  res.status(200).json({user})
-})
-
 router.get("/detail", auth, async (req, res) => {
-  
-  
+
   const userId = req.jwtPayload.user_id
   const user = await User.findByPk(userId, {
     include: [
