204bcc83188d1d0c31b6e04f8155e8c460b96792
tsukasa-forever <engineer.tsukasa@gmail.com>
2021-05-05 04:26:40 +0900

    add endpoint of user detail

--- routes/api/user.js
+++ routes/api/user.js
@@ -29,4 +29,27 @@ router.get("/:userId", auth, async (req, res) => {
   res.status(200).json({user})
 })
 
+router.get("/detail", auth, async (req, res) => {
+  
+  
+  const userId = req.jwtPayload.user_id
+  const user = await User.findByPk(userId, {
+    include: [
+      {
+        model: Event,
+      },
+      {
+        model: Event,
+        as: 'AdminEvent'
+      },
+    ],
+  })
+
+  if (!user) {
+    return res.status(404).json({message: "not found"})
+  }
+
+  res.status(200).json({user})
+})
+
 module.exports = router;
