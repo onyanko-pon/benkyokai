0f05aa12ed953b54d064ef5adf1bab44072827be
tsukasa-forever <engineer.tsukasa@gmail.com>
2021-05-04 11:41:44 +0900

    fix res

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   console.log({message: "cookie", cookie: req.cookies})
   const token = req.cookies.token;
   if (token === undefined || token === "") {
-    return res.status(403).json({message: "token not set"})
+    return res.status(403).json({message: "token not set", cookie: req.cookies})
   }
 
   jwt.verify(token, process.env.JWT_PRIVATE_KEY, function(err, payload) {
