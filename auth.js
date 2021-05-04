46b5920055b30c2524b129455c09f417a5b99bdb
tsukasa-forever <engineer.tsukasa@gmail.com>
2021-05-04 11:37:01 +0900

    add console

--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 
 // TODO next()を用いたスマートなやり方模索
 module.exports = (req, res, next) => {
+  console.log({message: "cookie", cookie: req.cookies})
   const token = req.cookies.token;
   if (token === undefined || token === "") {
     return res.status(403).json({message: "token not set"})
